0d9c1c81be3791e7124f72c0887e9d21b34c14df
Aimyon36 <2547373675@qq.com>
2023-03-07 20:40:53 +0800

    IDEA commit

---
+++ src/main/java/Aimyon/GitTest.java
@@ -0,0 +1,11 @@
+package Aimyon;
+
+/**
+ * @Date: 2023-03-07-20:35
+ * @Description:
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Aimyon36 ");
+    }
+}
