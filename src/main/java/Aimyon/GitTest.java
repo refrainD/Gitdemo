d249f467d6a3eac5d9dcb2eb572ddbf36cf13ca6
Aimyon36 <2547373675@qq.com>
2023-03-08 14:28:09 +0800

    hot fix commit

--- src/main/java/Aimyon/GitTest.java
+++ src/main/java/Aimyon/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("Aimyon36 ");
         System.out.println("hello");
+        System.out.println("i realy love you");
     }
 }
